9d228a1114610dc56455afae0be627e117af6e6b
YuneeeM <dana512@naver.com>
2023-09-19 17:21:14 +0900

    0919 - 6장 글 생성, 조회까지

--- src/main/java/me/yuni/springboot3/DAO/Member.java
+++ src/main/java/me/yuni/springboot3/DAO/Member.java
@@ -7,7 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //기본 생성자
 @AllArgsConstructor
 @Getter
 @Entity
